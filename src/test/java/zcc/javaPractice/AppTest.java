b1b9551d6366cbc3f84b6330f470706d7230ea86
zcc6248 <624875431@qq.com>
2021-11-10 17:59:41 +0800

    spring学习

--- src/test/java/zcc/javaPractice/AppTest.java
+++ src/test/java/zcc/javaPractice/AppTest.java
@@ -2,16 +2,18 @@ package zcc.javaPractice;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-import zcc.javaPractice.server.UserServer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import zcc.javaPractice.AOPandIOC.server.UserServer;
 
 public class AppTest 
 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ca = new ClassPathXmlApplicationContext("spring.xml");
-        UserServer userServer = (UserServer) ca.getBean("userServer");
-        userServer.text();
+//        UserServer userServer = (UserServer) ca.getBean("userServer");
+//        userServer.text();
+        JdbcTemplate JdbcTemplate = (JdbcTemplate) ca.getBean("jdbcTemplate");
+        Integer count = JdbcTemplate.queryForObject("select count(*) from user", Integer.class);
+        System.out.println("总数:" + count);
     }
 }
